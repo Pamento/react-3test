b5d87db194c18109e216cb1d63d00e7d672e9749
pawel <pohos.samut@gmail.com>
2019-02-25 11:51:39 +0100

    snaphsot test

---
+++ src/unit.test.js
@@ -0,0 +1,33 @@
+//src/unit.test.js
+//run test with npm test
+import React from 'react'
+
+
+
+function sum(a, b){
+    return a + b;
+}
+
+describe('sum', function () {
+
+});
+
+
+function arrayify(input = []) {
+    return arrayify.isArray(input) ? input : [input];
+}
+
+describe('arrayify', function () {
+    it('returns an empty array when given nothing', () => {
+
+    });
+
+    it('returns the array-ed version of what it is given', () => {
+
+    });
+
+    it('returns the array if it is given an array', () => {
+
+    });
+
+});
